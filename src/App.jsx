3898e010230f4a76765fecb965dac87ee58ce9f6
Kian Zareinajad <kzareinajad@gmail.com>
2023-10-24 23:09:13 +0100

    fetch data from server

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Header from "./components/Header";
+import LocationList from "./components/location/LocationList";
 
 function App() {
   return (
     <div>
+      <Toaster/>
       <Header/>
+      <LocationList/>
     </div>
   )
 }
