4ff81b58dfc087a9b4079e993a49a1dcb2cb751a
Kian Zareinajad <kzareinajad@gmail.com>
2023-11-14 02:21:17 +0000

    current hotel as context

--- src/components/singleHotel/SingleHotel.jsx
+++ src/components/singleHotel/SingleHotel.jsx
@@ -1,20 +1,28 @@
 import { useParams } from "react-router-dom";
-import useFetch from "../../hooks/useFetch";
 import Loader from "../Loader/Loader";
+import { useHotels } from "../context/HotelsProvider";
+import { useEffect } from "react";
 
 function SingleHotel() {
   const { id } = useParams();
-  const { data, isLoading } = useFetch(`http://localhost:5000/hotels/${id}`)
-  if(isLoading) return <Loader/>
+  const { getHotel, isLoadingCurrentHotel, currentHotel } = useHotels();
+  useEffect(() => {
+    getHotel(id);
+  }, [id]);
+
+  if (isLoadingCurrentHotel || !currentHotel) return <Loader />;
+
   return (
-  <div className="room">
-    <div className="roomDetail">
-        <h2>{data.name}</h2>
-        <div>{data.number_of_reviews} reviews &bull; {data.smart_location}</div>
-        <img src={data.xl_picture_url} alt={data.name} />
+    <div className="room">
+      <div className="roomDetail">
+        <h2>{currentHotel.name}</h2>
+        <div>
+          {currentHotel.number_of_reviews} reviews &bull; {currentHotel.smart_location}
+        </div>
+        <img src={currentHotel.xl_picture_url} alt={currentHotel.name} />
+      </div>
     </div>
-  </div>
-    )
+  );
 }
 
 export default SingleHotel;
